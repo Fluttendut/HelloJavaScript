574af08ae41a35f7b2653b4d9e07dad36f499d76
Fluttendut <ded_19@hotmail.com>
2023-04-17 11:29:12 +0200

    første

--- js/Hello.js
+++ js/Hello.js
@@ -1,2 +1,9 @@
-// lars
-// lars
+
+function sayHello() {
+
+    document.querySelector("h3").textContent = "Hej" + document.querySelector("input").value;
+    //Anvend querySelector til at få fat i h3-elementet og igen querySelector til at få fat i værdien fra input-elementet:
+}
+
+document.querySelector("button").addEventListener("click", sayHello);
+//Knyt et click-event til button-elementet. På “click” kaldes funktionen sayHello:
