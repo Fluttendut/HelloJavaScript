d9fe8d775b6fd121108c616996ac4a5fab91faa1
Fluttendut <ded_19@hotmail.com>
2023-04-17 11:53:31 +0200

    Changes by Benjamin

--- js/Hello.js
+++ js/Hello.js
@@ -3,6 +3,9 @@ function sayHello() {
 
     // document.querySelector("h3").textContent = "Hej " + document.querySelector("input").value;
     //Anvend querySelector til at få fat i h3-elementet og igen querySelector til at få fat i værdien fra input-elementet:
+
+    document.querySelector("#firstLine").textContent = "Hej " + document.querySelector("#name").value;
+    document.querySelector("#secondLine").textContent = document.querySelector("#age").value
 }
 
 document.querySelector("button").addEventListener("click", sayHello);
